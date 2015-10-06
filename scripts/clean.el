ad1a10aad7deb281b7b93c74d1906a5b81681d76
Saul Shanabrook <s.shanabrook@gmail.com>
2015-10-05 21:27:48 -0400

    first try at clean command

--- scripts/clean.el
+++ scripts/clean.el
@@ -1,4 +1,8 @@
+(require 'cl)
+(toggle-debug-on-error)
+(package-initialize)
 (require 'clj-refactor)
 
+(setq cljr-project-project-clean-functions (list 'cljr-clean-ns))
 ;;(setq cljr-project-clean-prompt nil)
 (cljr-project-clean)
